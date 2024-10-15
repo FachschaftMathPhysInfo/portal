230095828b3f10c174d4f58c9047badcb556ae90
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2024-10-15 17:01:50 +0200

    change theme switch to button

--- components/themeSwitch.tsx
+++ components/themeSwitch.tsx
@@ -1,8 +1,7 @@
 'use client'
 import {useEffect, useState} from "react";
 import {useTheme} from "next-themes";
-import {Switch} from "./ui/switch"
-import {resolve} from "dns";
+import Image from "next/image";
 
 const ThemeSwitch = () => {
   const [mounted, setMounted] = useState(false)
@@ -18,12 +17,19 @@ const ThemeSwitch = () => {
   }
 
   return (
-    <Switch
-      onCheckedChange={() => {
+    <button
+      id={'theme-switch'}
+      className={'p-0 bg-transparent'}
+      onClick={() => {
         currentTheme === 'dark' ? setTheme('light') : setTheme('dark');
       }}
-      checked={typeof currentTheme === "undefined" ? false : currentTheme === 'dark'}
-    />
+    >
+      <Image
+        src={currentTheme === 'dark' ? '/icons_light/theme_toggle.svg' : '/icons_dark/theme_toggle.svg'}
+        alt={''}
+        width={currentTheme === 'dark' ? 40 : 35}
+        height={currentTheme === 'dark' ? 40 : 35}/>
+    </button>
   )
 }
 
