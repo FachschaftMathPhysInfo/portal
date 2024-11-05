6d86b4568399999578c9db05d8cf8cc8f819b833
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2024-11-05 19:33:19 +0100

    quickfix for views

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import AppCard from "@/components/appCard";
 import {appDataCollection} from "@/lib/data/appData";
 import {connectStyles} from "@/lib/utils/stylingConnector";
 
-const baseStyling: string = 'grid p-16 w-fit h-fit mx-auto min-w-[20rem] grid-cols-2 gap-6 gap-y-6';
+const baseStyling: string = 'grid p-10 w-fit h-fit mx-auto min-w-[20rem] grid-cols-2 gap-6';
 const mdStyling: string = 'lg:grid-cols-3';
-const xlStyling: string = 'xl:grid-cols-4 xl:gap-5';
+const xlStyling: string = 'xl:grid-cols-4';
 
 export default function Home() {
   return (
-    <main className={'xl:h-[100vh] w-[100vw] p-0 m-0 overflow-y-scroll'}>
+    <main className={'xl:h-[calc(100vh-5rem] w-[100vw] p-0 m-0 overflow-y-scroll'}>
       <Header/>
       <div className={'w-full h-full flex items-center justify-center'} >
         <div id='app-container' className={connectStyles(baseStyling, mdStyling, xlStyling)}>
